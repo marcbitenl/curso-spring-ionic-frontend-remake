968fe353e57ae984a52ee48d225557fb33d7d843
marcbitenl <marcbitenl@gmail.com>
2020-07-04 21:08:47 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-  baseUrl : 'http://localhost:8084'
+  baseUrl : 'http://localhost:8084',
+  bucketBaseUrl: 'https://curso-spring-ionic-mbl.s3-sa-east-1.amazonaws.com'
 }
