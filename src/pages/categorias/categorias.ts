78f5806de5b2e24a849a178a24f390a74e0bac9e
marcbitenl <marcbitenl@gmail.com>
2020-07-05 18:18:57 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -4,12 +4,6 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
 
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -33,9 +27,7 @@ export class CategoriasPage {
     .subscribe(response => {
       this.items = response;
     },
-    error => {
-      console.log(error);
-    });
+    error => {});
 
 
   }
