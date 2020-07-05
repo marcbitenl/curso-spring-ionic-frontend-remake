4d794290d27fa79c61bb3db259d908eef1d49426
marcbitenl <marcbitenl@gmail.com>
2020-07-05 19:04:05 -0300

    Salvando os dados do usuário logado no localStorage

---
+++ src/services/storage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { LocalUser } from "../models/local_user";
+import { STORAGE_KEYS } from "../config/storage_keys.config";
+
+@Injectable()
+export class StorageService {
+
+  getLocalUser(): LocalUser{
+    let usr = localStorage.getItem(STORAGE_KEYS.localuser);
+    if(usr == null) {
+      return null;
+    }
+    else {
+      return JSON.parse(usr);
+    }
+  }
+
+  setLocalUser(obj : LocalUser){
+    if(obj == null){
+      localStorage.removeItem(STORAGE_KEYS.localuser);
+    }
+    else{
+      localStorage.setItem(STORAGE_KEYS.localuser, JSON.stringify(obj));
+    }
+
+
+  }
+
+}
