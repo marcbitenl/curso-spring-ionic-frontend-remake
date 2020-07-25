c8ba8798013bbdf0c887b7113dc9a38ba04a716c
marcbitenl <marcbitenl@gmail.com>
2020-07-25 17:51:53 -0300

    Infinite scroll

--- src/services/produto.service.ts
+++ src/services/produto.service.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { ProdutoDTO } from '../models/produto.dto';
 import { API_CONFIG } from '../config/api.config';
 
-
 @Injectable()
 export class ProdutoService {
 
@@ -17,8 +16,8 @@ export class ProdutoService {
     return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
   }
 
-  findByCategoria(categoria_id : string) {
-    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  findByCategoria(categoria_id : string, page : number = 0, linesPerPage : number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
   }
 
   getSmallImageFromBucket(id : string) : Observable<any> {
